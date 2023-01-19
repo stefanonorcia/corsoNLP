c53596f4cd2e5cc37a86ed4f1f3ac541d552bb24
Stefano Norcia <stefano.norcia@gmail.com>
2023-01-19 09:50:59 +0100

    fix electrivity py

--- main/ds/electricity.py
+++ main/ds/electricity.py
@@ -1,10 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("electricity.csv",
-                 header=52608,
-                 parse_dates=True,
-                 index_col=0)
+df = pd.read_csv("electricity.csv", header=52608, parse_dates=True, index_col=0)
 df.plot()
 
 plt.grid()
